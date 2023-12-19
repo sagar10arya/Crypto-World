e2c0cfc51187fc7110407af7ec6fe9c73d7c07de
Sagar Arya <71361360+sagar10arya@users.noreply.github.com>
2023-12-19 23:15:55 +0530

    Add files via upload

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,44 @@
+
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// Note: Change v1 to v2 on rapid api
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+};
+
+const baseUrl = process.env.REACT_APP_CRYPTO_API_URL;
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+
+    // Note: Change the coin price history endpoint from this - `coin/${coinId}/history/${timeperiod} to this - `coin/${coinId}/history?timeperiod=${timeperiod}`
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+    }),
+
+    // Note: To access this endpoint you need premium plan
+    getExchanges: builder.query({
+      query: () => createRequest('/exchanges'),
+    }),
+  }),
+});
+
+export const {
+  useGetCryptosQuery,
+  useGetCryptoDetailsQuery,
+  useGetExchangesQuery,
+  useGetCryptoHistoryQuery,
+} = cryptoApi;
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,24 @@
+import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsApiHeaders = {
+    'X-BingApis-SDK': 'true',
+    'X-RapidAPI-Key': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
+    'X-RapidAPI-Host': process.env.REACT_APP_NEWS_RAPIDAPI_HOST
+}
+
+const baseUrl = process.env.REACT_APP_NEWS_API_URL ;
+
+const createRequest = (url) => ({url, headers:cryptoNewsApiHeaders})
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+            query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+        })
+    })
+});
+
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
